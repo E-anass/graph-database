904c9cbaafec0cbb2070c0024339e27673cf0ed9
aijen <jennahaimen@gmail.com>
2020-04-26 14:00:50 +0200

    add mysql µService

--- graph-front/src/main/web/src/app/app.module.ts
+++ graph-front/src/main/web/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './layout/main-menu/main-menu.component';
+import { SideBarComponent } from './layout/side-bar/side-bar.component';
+import { ContentComponent } from './content/content.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent,
+    SideBarComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
