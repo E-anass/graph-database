16df56fea0c5f3fe6f5d33004fd90651094ba21b
aijen <jennahaimen@gmail.com>
2020-04-26 06:42:26 +0200

    maven configure front end

---
+++ graph-front/src/test/java/com/angular/graphfront/GraphFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angular.graphfront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
