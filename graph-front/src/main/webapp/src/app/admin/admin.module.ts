87cc618da77b47e7b6bdf263cc1e55e6192dbf42
aijen <jennahaimen@gmail.com>
2020-04-29 19:55:01 +0200

    customiza a template

---
+++ graph-front/src/main/webapp/src/app/admin/admin.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {SchedulesComponent} from "./schedules/schedules.component";
+
+
+@NgModule({
+  declarations: [
+    SchedulesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
